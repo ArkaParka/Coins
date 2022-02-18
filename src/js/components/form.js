9b3afb184038fa67d05ca10952ac381562d4bbec
Arina Perkon <masjandrikovna2.0@rambler.ru>
2022-02-18 12:17:48 +0300

    form

---
+++ src/js/components/form.js
@@ -0,0 +1,22 @@
+import 'select2';
+
+$(".form-item__select").select2({
+	// theme: "classic",
+	minimumResultsForSearch: -1,
+});
+
+// function init() {
+// 	$(document).ready(function() {
+// 		$('.form-item__select').select2({
+// 			placeholder: 'Select an option'
+// 		});
+// 	});
+//
+// }
+
+// export default {
+// 	init,
+// 	destroy,
+// 	openMenu,
+// 	closeMenu,
+// };
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import './vendor';
 import './helpers';
 import './components/social';
+import './components/form';
 import {ieFix} from './vendor/ie-fix';
 import {vhFix} from './vendor/vh-fix';
 import {actualYear} from './modules/actualYear';
 import header from './components/header';
+import form from './components/form';
 import lazyLoading from './modules/lazyLoading';
 import scrollToAnchor from './modules/scrollToAnchor';
 
